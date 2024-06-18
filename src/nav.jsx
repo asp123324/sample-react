fee9a71af5a0967476557611c8eb5c221dd371a9
asp123324 <ashwin.puthenpura@moonhive.in>
2024-06-18 14:12:36 +0530

    ss

--- src/nav.jsx
+++ src/nav.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 function Nav() {
   return (
     <nav>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-      
+      <a href="#home" style={{ marginRight: '30px' }}>Home</a>
+      <a href="#about" style={{ marginRight: '30px' }}>About</a>
       <a href="#contact">Contact</a>
     </nav>
   );
